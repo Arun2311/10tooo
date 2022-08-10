b5666acb5f32aef238bf0a0c81d62d6f3f09a585
Arunachalam <cvarunachalam84@gmail.com>
2022-08-10 11:56:03 +0530

    first commit

---
+++ Script.js
@@ -0,0 +1,37 @@
+// let array = [2,3,4,1,1,2]
+
+
+// const m = array.map(a => a*2)
+
+// console.log(m);
+
+// let a = [5,2,34,2,20,30,2]
+
+// const fin = a.find(x => x>20)
+
+// console.log(fin);
+
+
+// let a = [5,2,34,2,20,30,2]
+
+// const fin = a.filter(x => x>20)
+
+// console.log(fin);
+
+
+// const months = ["jan","march","feb","august","nov"]
+
+// months.splice(0,1,'dec')
+
+// console.log(months);
+
+
+
+// HTML --- apple
+
+// css -- beetroot
+
+// js - carrot
+
+
+// abc = react Js
